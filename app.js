a8c6498150362d43d5523d43fd13dcfe9ed096cb
Jayprecode <jayprecode@gmail.com>
2021-01-24 09:20:01 +0100

    comments & code simplicity

--- app.js
+++ app.js
@@ -5,55 +5,69 @@ document.getElementById("loan-form").addEventListener("submit", function (e) {
     //Show loader
     document.getElementById("loading").style.display = "block";
 
-    setTimeout(calculateResults, 3200);
+	setTimeout(calculateLoanResults, 3200);
+	//Prevents browser from refreshing
         e.preventDefault();
-    });
+});
+	
 //calculate Results
-function calculateResults() {
+function calculateLoanResults() {
 	console.log("calculating...");
-	//UI Vars
-	const amount = document.getElementById("amount");
-	const interest = document.getElementById("interest");
-	const years = document.getElementById("years");
-	const monthlyPayment = document.getElementById("monthly-payment");
-	const totalPayment = document.getElementById("total-payment");
-	const totalInterest = document.getElementById("total-interest");
 
-	const principal = parseFloat(amount.value);
-	const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-	const calculatedPayments = parseFloat(years.value) * 12;
+	const a = document.getElementById("amount"),
+	   	i = document.getElementById("interest"),
+		y = document.getElementById("years"),
+	 	mp = document.getElementById("monthly-payment"),
+	 	tp = document.getElementById("total-payment"),
+	 	ti = document.getElementById("total-interest");
+
+	//Principal
+	//parseFloat turns the value to decimal
+	const p = parseFloat(a.value);
+	//calculatedInterest
+	const ci = parseFloat(i.value) / 100 / 12;
+	//calculatePayments
+	const cps = parseFloat(y.value) * 12;
 
 	//Compute the monthly payment
-	const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-	const monthly = (principal * x * calculatedInterest) / (x - 1);
+	const x = Math.pow(1 + ci, cps),
+	//Monthly
+	 m = (p * x * ci) / (x - 1);
 
-	if (isFinite(monthly)) {
-		monthlyPayment.value = monthly.toFixed(2);
-		totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-		totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+	if (isFinite(m)) {
+		//To fixed make us set the amount of decimal e.g 435.94 
+		//if it's set to three....it's going to be e.g 435.943
+		mp.value = m.toFixed(2);
+		tp.value = (m * cps).toFixed(2);
+		ti.value = (m * cps - p).toFixed(2);
 
 		//show results
 		document.getElementById("results").style.display = "block";
 		//Hide loader
 		document.getElementById("loading").style.display = "none";
+	} else if (a.value == ("")  ){
+		showError("Loan amount cannot be blank !");
+	} else if (i.value == ("") ) {
+		showError("Interest required !");
+	} else if (y.value == ("") ) {
+		showError("Insert years of payment");
 	} else {
 		showError("Please check your numbers");
-	}
-}
+	};
+};
 
-//Showw Error
-function showError(error) {
-	//Get Elements
-	const card = document.querySelector(".card");
-	const heading = document.querySelector(".heading");
+//Show Error
+function showError(err) {
+	const c = document.querySelector(".card"),
+		h = document.querySelector(".heading");
 	//Create a div
-	const errorDiv = document.createElement("div");
+	const error = document.createElement("div");
 	//Add class
-	errorDiv.className = "alert";
+	error.className = "alert";
 	// Create text Node and append to div
-	errorDiv.appendChild(document.createTextNode(error));
+	error.appendChild(document.createTextNode(err));
 	//Insert error above heading
-	card.insertBefore(errorDiv, heading);
+	c.insertBefore(error, h);
 	//Clear error after 3 seconds
 	setTimeout(clearError, 3000);
 
