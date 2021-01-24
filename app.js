b7afabad3d956526ae415e614233468d365c6ca4
Jayprecode <jayprecode@gmail.com>
2021-01-24 01:03:40 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+//Listen for submit
+document.getElementById("loan-form").addEventListener("submit", function (e) {
+    //Hide results
+    document.getElementById("results").style.display = "none";
+    //Show loader
+    document.getElementById("loading").style.display = "block";
+
+    setTimeout(calculateResults, 3200);
+        e.preventDefault();
+    });
+//calculate Results
+function calculateResults() {
+	console.log("calculating...");
+	//UI Vars
+	const amount = document.getElementById("amount");
+	const interest = document.getElementById("interest");
+	const years = document.getElementById("years");
+	const monthlyPayment = document.getElementById("monthly-payment");
+	const totalPayment = document.getElementById("total-payment");
+	const totalInterest = document.getElementById("total-interest");
+
+	const principal = parseFloat(amount.value);
+	const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+	const calculatedPayments = parseFloat(years.value) * 12;
+
+	//Compute the monthly payment
+	const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+	const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+	if (isFinite(monthly)) {
+		monthlyPayment.value = monthly.toFixed(2);
+		totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+		totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+
+		//show results
+		document.getElementById("results").style.display = "block";
+		//Hide loader
+		document.getElementById("loading").style.display = "none";
+	} else {
+		showError("Please check your numbers");
+	}
+}
+
+//Showw Error
+function showError(error) {
+	//Get Elements
+	const card = document.querySelector(".card");
+	const heading = document.querySelector(".heading");
+	//Create a div
+	const errorDiv = document.createElement("div");
+	//Add class
+	errorDiv.className = "alert";
+	// Create text Node and append to div
+	errorDiv.appendChild(document.createTextNode(error));
+	//Insert error above heading
+	card.insertBefore(errorDiv, heading);
+	//Clear error after 3 seconds
+	setTimeout(clearError, 3000);
+
+	//Hide results
+	document.getElementById("results").style.display = "none";
+	//Hide loader
+	document.getElementById("loading").style.display = "none";
+}
+//Clear error
+function clearError() {
+	document.querySelector(".alert").remove();
+}
